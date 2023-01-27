ab1a22e3b84d071e29ca64114c8764aad5dfa767
Federico Ruggi <1081051+ruggi@users.noreply.github.com>
2023-01-27 11:04:07 +0100

    bootstrap

---
+++ utopia/storyboard.js
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import { Scene, Storyboard } from 'utopia-api'
+import { App } from '/src/app.js'
+import { Playground } from '/src/playground.js'
+
+export var storyboard = <Storyboard />
